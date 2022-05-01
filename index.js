51c1c5c6928c7abaf84eaadbf187dd8491b4498f
ismail-hossen <satict2016@gmail.com>
2022-05-01 11:24:00 +0600

    implement delete api

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete one item by id parameter
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log("connected to db");
   } finally {
   }
